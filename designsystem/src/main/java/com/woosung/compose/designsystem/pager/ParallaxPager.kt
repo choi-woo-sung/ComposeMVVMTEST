e8b31dc62ed264c892a4797163f28e67eebc7b6c
최우성 <vov1252@naver.com>
2023-05-09 20:20:08 +0900

    [Remove] extension  common으로 옮기기  및 이름 변경

--- designsystem/src/main/java/com/woosung/compose/designsystem/pager/ParallaxPager.kt
+++
@@ -1,7 +0,0 @@
-package com.woosung.compose.designsystem.pager
-
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.runtime.Composable
-import coil.compose.AsyncImage
-
